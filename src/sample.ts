c64bcd50693fd4163b825c560a5281023064af97
aiinuuazzariaa <ainu.azzaria@gmail.com>
2026-05-16 18:13:46 +0700

    first commit

---
+++ src/sample.ts
@@ -0,0 +1,15 @@
+const author = {
+  name: `Ainu Azzaria`,
+  nickname: `ainu`,
+  tiktok: `https://www.tiktok.com/@indieteamofone`,
+  ig: `https://www.instagram.com/indieteamofone/`,
+  x: `https://x.com/indieteamofone`,
+  email: "mailto:ainu.azzaria@gmail.com",
+};
+
+const buy = {
+  title: `See on Github`,
+  link: `https://github.com/aiinuuazzariaa`,
+};
+
+export { author, buy };
