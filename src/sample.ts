7e297f8ef43a20bf2adbd80df9e3dbfe80f508e9
aiinuuazzariaa <ainu.azzaria@gmail.com>
2026-05-17 14:48:28 +0700

    upd: update works

--- src/sample.ts
+++ src/sample.ts
@@ -2,13 +2,13 @@ const author = {
   name: `Ainu Azzaria`,
   nickname: `ainu`,
   tiktok: `https://www.tiktok.com/@indieteamofone`,
-  ig: `https://www.instagram.com/indieteamofone/`,
+  ig: `https://www.instagram.com/aiinuuazzariaa/`,
   x: `https://x.com/indieteamofone`,
   email: "mailto:ainu.azzaria@gmail.com",
 };
 
 const buy = {
-  title: `Github Saya`,
+  title: `My Github`,
   link: `https://github.com/aiinuuazzariaa`,
 };
 
